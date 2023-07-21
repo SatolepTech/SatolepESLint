e02e1b5ab9638a765683b97583e66f5d459b2c02
Bruno Ferraz Silveira <bfssfb@outlook.com.br>
2023-07-21 18:44:05 -0300

    feat: add ReactNative ESLint config

---
+++ react-native.js
@@ -0,0 +1,98 @@
+module.exports = {
+  extends: [
+    '@react-native-community',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/recommended',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended'
+  ],
+  ignorePatterns: ['node_modules', 'src/generated'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off'
+      }
+    }
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'prettier',
+    'simple-import-sort',
+    'unused-imports'
+  ],
+  root: true,
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'arrow-body-style': 'off',
+    'eslint-comments/no-unlimited-disable': 'off',
+    'import/named': 'off',
+    'import/namespace': 'off',
+    'import/no-unresolved': 'off',
+    'import/order': 'off',
+    'no-useless-constructor': ['off'],
+    'prefer-arrow-callback': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'avoid',
+        bracketSameLine: false,
+        bracketSpacing: true,
+        endOfLine: 'auto',
+        jsxSingleQuote: true,
+        printWidth: 70,
+        semi: false,
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'none'
+      },
+      {
+        usePrettierrc: false
+      }
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^\\u0000'],
+          ['^react$'],
+          ['^@assets(/.*|$)', '^@atomic(/.*|$)'],
+          ['^@common(/.*|$)', '^@config(/.*|$)', '^@routes(/.*|$)'],
+          ['^@src(/.*|$)'],
+          ['^@\\w'],
+          ['^\\.']
+        ]
+      }
+    ],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ],
+    'sort-imports': 'off',
+    'unused-imports/no-unused-imports': 'warn',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_'
+      }
+    ]
+  }
+}
